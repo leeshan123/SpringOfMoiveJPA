ee66756fe8cb716cee01f270da33fe98c3a31bbb
AwesomeMinsik <beauty3579@gmail.com>
2024-04-01 18:46:21 +0900

    커뮤니티 페이지 추가,기능구현예정

---
+++ src/main/java/kr/co/moviespring/web/controller/CommunityController.java
@@ -0,0 +1,16 @@
+package kr.co.moviespring.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("community")
+public class CommunityController {
+
+    @GetMapping("board")
+    public String board(){
+        return "community/board";
+    }
+    
+}
