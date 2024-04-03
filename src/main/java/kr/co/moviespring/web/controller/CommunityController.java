962af58fcfd09729acb94c102f0632e1a4f184b5
Java-ChipFrappuccino <pss21242124@gmail.com>
2024-04-03 16:11:55 +0900

    ver 0.0.20 게시글 작성페이지 작성

--- src/main/java/kr/co/moviespring/web/controller/CommunityController.java
+++ src/main/java/kr/co/moviespring/web/controller/CommunityController.java
@@ -3,6 +3,8 @@ package kr.co.moviespring.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("community")
@@ -11,6 +13,11 @@ public class CommunityController {
     @GetMapping("main")
     public String main(){
         return "community/main";
+    }
+    
+    @GetMapping("reg")
+    public String reg() {
+        return "community/reg";
     }
     
 }
