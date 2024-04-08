966923f17d740014c2e0d3ccfeb119c0d022bdcf
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-04-08 18:10:40 +0900

    ver. 0.2.2 / id, pwd 찾기 페이지 추가

--- src/main/java/kr/co/moviespring/web/controller/MemberController.java
+++ src/main/java/kr/co/moviespring/web/controller/MemberController.java
@@ -25,6 +25,18 @@ public class MemberController {
         return "user/signin";
     }
 
+    @GetMapping("findId")
+    public String findId() {
+
+        return "user/findId";
+    }
+
+    @GetMapping("findPwd")
+    public String findPwd() {
+
+        return "user/findPwd";
+    }
+
     //스프링 사큐리티 설정파일의 .loginProcessingUrl("/user/signin")으로 로그인 폼데이터를 POST요청받음
     /*@PostMapping("signin")
     public String signin(String username, String password, HttpSession session, HttpServletResponse response) {
