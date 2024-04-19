5509e33eba919aa4df4c91013d7d33b327e84c7e
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-04-19 17:04:06 +0900

    ver. 0.4.4 / kobis 영화상세정보 추가

---
+++ src/main/java/kr/co/moviespring/web/movieapi/KonisMovieInfoEntity/Audits.java
@@ -0,0 +1,20 @@
+package kr.co.moviespring.web.movieapi.KonisMovieInfoEntity;
+
+//심의정보
+public class Audits {
+    private String auditNo;	        //심의번호
+    private String watchGradeNm;	//관람등급 명칭
+    
+    public String getAuditNo() {
+        return auditNo;
+    }
+    public void setAuditNo(String auditNo) {
+        this.auditNo = auditNo;
+    }
+    public String getWatchGradeNm() {
+        return watchGradeNm;
+    }
+    public void setWatchGradeNm(String watchGradeNm) {
+        this.watchGradeNm = watchGradeNm;
+    }
+}
